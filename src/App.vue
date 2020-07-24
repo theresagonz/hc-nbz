e1f398ede8a2d9763364803a65510d7fd1518e87
theresamorelli <theresa.l.morelli@gmail.com>
2020-07-23 20:28:38 -0700

    First commit, add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+body {
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-color: #333;
+
+}
+a {
+  color: white;
+}
+hr {
+  box-shadow: 0.1em 0.1em #000;
+}
+.animate {
+  -webkit-transition: all 0.2s ease-out;
+  -moz-transition: all 0.2s ease-out;
+  -ms-transition: all 0.2s ease-out;
+  -o-transition: all 0.2s ease-out;
+  transition: all 0.2s ease-out;
+}
+.active {
+  color: lime;
+  opacity: 1 !important;
+}
+.text {
+  float: left;
+  padding: 0em 1em 0em 1em;
+  text-align: justify;
+  color: white;
+  text-shadow: 0.2em 0.2em 0.1em #000;
+}
+#findPlayer {
+  width: 100%;
+  float: left;
+  color: white;
+  text-shadow: 0.2em 0.2em 0.1em #000;
+}
+#findPlayer span {
+  margin-left: 0.5em;
+  opacity: 0.5;
+  font-weight: bold;
+}
+#findPlayer input {
+  max-width: 10em;
+  height: 2em;
+  border-radius: 0.2em;
+}
+#tester {
+  position: fixed;
+  bottom: 1em;
+  right: 1em;
+  background: #000;
+  color: green;
+  font-weight: bold;
+  width: 20em;
+  height: 6em;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- Links (sit on top) -->
+  <div class="w3-top">
+    <div class="navbar w3-row w3-large w3-dark-grey">
+      <div class="nav-item w3-col w3-third">
+        <a href="#" class="w3-button w3-block animate">Home</a>
+      </div>
+      <div class="nav-item w3-col w3-third">
+        <a href="#plans" class="w3-button w3-block animate">Plans</a>
+      </div>
+      <div class="nav-item w3-col w3-third">
+        <a href="#aboutus" class="w3-button w3-block animate">About Us</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String,
+  },
+};
+</script>
